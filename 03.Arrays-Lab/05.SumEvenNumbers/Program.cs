e8b008f22ed1d3165f25e240ce9043a7f7dff834
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-13 12:52:00 +0200

    Add files via upload

---
+++ 03.Arrays-Lab/05.SumEvenNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SumEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int currentNumber = nums[i];
+                if (currentNumber % 2 == 0)
+                {
+                    sum += currentNumber;
+                }
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
