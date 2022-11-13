6c7d7ee691fb10186b6612792b20fe265a378519
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-13 14:05:15 +0200

    Add files via upload

---
+++ 06.Methods-Exercises/09.PalindromeIntegers/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace PalindromeIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                int currentNumber = int.Parse(input);
+                string result = IsPalindrome(currentNumber).ToString().ToLower();
+
+                Console.WriteLine(result);
+
+                input = Console.ReadLine();
+            }
+        }
+
+        public static bool IsPalindrome(int x)
+        {
+            if (x < 0)
+            {
+                return false;
+            }
+
+            int div = 1;
+
+            while (x / div >= 10)
+            {
+                div *= 10;
+            }
+
+            while (x != 0)
+            {
+                int i = x / div;
+                int r = x % 10;
+
+                if (i != r)
+                {
+                    return false;
+                }
+
+                x = (x % div) / 10;
+                div /= 100;
+            }
+            return true;
+        }
+    }
+}
