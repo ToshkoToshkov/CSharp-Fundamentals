b9cebbaae61b3b1bb181b873556f9333ae7d5177
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-13 13:47:56 +0200

    Add files via upload

---
+++ 05.Methods-Lab/05.CalculateRectangleArea/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CalculateRectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double weight = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            double area = GetRectangleArea(weight, height);
+            Console.WriteLine(area);
+        }
+
+        static double GetRectangleArea(double weight, double height)
+        {
+            return weight * height;
+        }
+    }
+}
