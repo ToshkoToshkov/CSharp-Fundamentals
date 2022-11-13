5bf2ec6588f438f4c6f0f9c051f1d5ec78c26627
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-13 14:01:22 +0200

    Add files via upload

---
+++ 06.Methods-Exercises/01.SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace SmallestOfThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(SmallestInt(first, second, third));
+
+        }
+
+        private static int SmallestInt(int first, int second, int third)
+        {
+            if (first < second && first < third)
+            {
+                return first;
+            }
+            else if (second < first && second < third)
+            {
+                return second;
+            }
+            else
+            {
+                return third;
+            }
+        }
+    }
+}
