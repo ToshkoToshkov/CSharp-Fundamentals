65e0cf42da6112ca7eb1e29ce0753a0581f7502b
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-13 14:03:04 +0200

    Add files via upload

---
+++ 06.Methods-Exercises/05.AddAndSubtract/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace AddAndSubtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+
+            int sum = Sum(first, second, third);
+            Console.WriteLine(sum);
+        }
+
+
+        private static int Sum(int first, int second, int third)
+        {
+            int result = first + second;
+            return Subtract(result, third);
+        }
+
+        private static int Subtract(int result, int third)
+        {
+            return result - third;
+        }
+    }
+}
