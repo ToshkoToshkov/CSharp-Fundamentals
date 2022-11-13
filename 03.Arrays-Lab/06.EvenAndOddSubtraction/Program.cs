e56050d1dcc89adedb451b139cee7c3d71087468
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-13 12:52:25 +0200

    Add files via upload

---
+++ 03.Arrays-Lab/06.EvenAndOddSubtraction/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace EvenAndOddSubtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int sumEven = 0;
+            int sumOdd = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int currentNumber = nums[i];
+
+                if (currentNumber % 2 == 0)
+                {
+                    sumEven += currentNumber;
+                }
+                else
+                {
+                    sumOdd += currentNumber;
+                }
+            }
+
+            int result = sumEven - sumOdd;
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
