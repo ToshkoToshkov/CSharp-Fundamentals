5eeba17c6787de4d5e4fd2695d1dc20afb0edf45
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-13 12:46:29 +0200

    Add files via upload

---
+++ 02.ConditionalStatementsAndLoops-Exercises/07.VendingMachine/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace VendingMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal totalMoney = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "Start")
+                {
+                    break;
+                }
+
+                decimal coin = decimal.Parse(input);
+
+                if (coin == 0.1m || coin == 0.2m || coin == 0.5m || coin == 1m || coin == 2m)
+                {
+                    totalMoney += coin;
+                }
+                else
+                {
+                    Console.WriteLine($"Cannot accept {coin}");
+                }
+            }
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+ 
+                if (input == "End")
+                {
+                    break;
+                }
+
+
+                if (input == "Nuts")
+                {
+                    if (totalMoney >= 2.0m)
+                    {
+                        totalMoney -= 2.0m;
+                        Console.WriteLine("Purchased nuts");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry, not enough money");
+                    }
+                }
+                else if (input == "Water")
+                {
+                    if (totalMoney >= 0.7m)
+                    {
+                        totalMoney -= 0.7m;
+                        Console.WriteLine("Purchased water");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry, not enough money");
+                    }
+                }
+                else if (input == "Crisps")
+                {
+                    if (totalMoney >= 1.5m)
+                    {
+                        totalMoney -= 1.5m;
+                        Console.WriteLine("Purchased crisps");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry, not enough money");
+                    }
+                }
+                else if (input == "Soda")
+                {
+                    if (totalMoney >= 0.8m)
+                    {
+                        totalMoney -= 0.8m;
+                        Console.WriteLine("Purchased soda");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry, not enough money");
+                    }
+                }
+                else if (input == "Coke")
+                {
+                    if (totalMoney >= 1.0m)
+                    {
+                        totalMoney -= 1.0m;
+                        Console.WriteLine("Purchased coke");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry, not enough money");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid product");
+                }
+            }
+
+            Console.WriteLine($"Change: {totalMoney:F2}");
+        }
+    }
+}
