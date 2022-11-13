bba2cf0a4ec55291fad946cfee217db426b256d5
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-13 13:46:19 +0200

    Add files via upload

---
+++ 05.Methods-Lab/01.SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SignOfIntegersNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            printSignIntNumber(n);
+        }
+
+        static void printSignIntNumber(int n)
+        {
+            if (n < 0)
+            {
+                Console.WriteLine($"The number {n} is negative.");
+            }
+            else if (n > 0)
+            {
+                Console.WriteLine($"The number {n} is positive.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {n} is zero.");
+            }
+        }
+    }
+}
