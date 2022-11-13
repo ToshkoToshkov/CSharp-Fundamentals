0a2fd66eb2dfdf29b25707af5adc4a2488b4a830
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-13 12:44:44 +0200

    Add files via upload

---
+++ 02.ConditionalStatementsAndLoops-Exercises/04.PrintAndSum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PrintAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int final = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = start; i <= final; i++)
+            {
+                Console.Write(i + " ");
+                sum += i;
+            }
+
+            int totalSum = sum;
+
+            Console.WriteLine("");
+            Console.WriteLine($"Sum: {totalSum}");
+        }
+    }
+}
