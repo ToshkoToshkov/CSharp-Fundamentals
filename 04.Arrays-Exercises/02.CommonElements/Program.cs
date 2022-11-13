d863ead4c61a075087c65e76aaad2c8a2f12c608
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-13 12:56:04 +0200

    Add files via upload

---
+++ 04.Arrays-Exercises/02.CommonElements/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CommonElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArray = Console.ReadLine().Split();
+
+            string[] secondArray = Console.ReadLine().Split();
+
+            foreach (string elementTwo in secondArray)
+            {
+                for (int i = 0; i < firstArray.Length; i++)
+                {
+                    if (elementTwo == firstArray[i])
+                    {
+                        Console.Write(elementTwo + " ");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
