91353c71d10c630882b76dae947ea1522f860333
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-13 14:01:42 +0200

    Add files via upload

---
+++ 06.Methods-Exercises/02.VowelsCount/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace VowelsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().ToLower();
+
+            int result = PrintVowols(input);
+
+            Console.WriteLine(result);
+        }
+
+        public static int PrintVowols(string input)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentChar = input[i];
+
+                if (currentChar == 'a')
+                {
+                    counter++;
+                }
+                else if (currentChar == 'e')
+                {
+                    counter++;
+                }
+                else if (currentChar == 'i')
+                {
+                    counter++;
+                }
+                else if (currentChar == 'o')
+                {
+                    counter++;
+                }
+                else if (currentChar == 'u')
+                {
+                    counter++;
+                }
+                else if (currentChar == 'y')
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+}
