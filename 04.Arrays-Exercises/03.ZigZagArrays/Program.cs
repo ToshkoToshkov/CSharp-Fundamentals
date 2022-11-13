fc3db42d34596963ac94c32d303bb0b5487e6408
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-13 12:56:25 +0200

    Add files via upload

---
+++ 04.Arrays-Exercises/03.ZigZagArrays/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ZigZagArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] firstArr = new string[n];
+            string[] secondArr = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] currentArray = Console.ReadLine()
+                    .Split()
+                    .ToArray();
+
+                string indexZeroElement = currentArray[0];
+                string indexFirstElement = currentArray[1];
+
+                if (i % 2 == 0)
+                {
+                    firstArr[i] = indexZeroElement;
+                    secondArr[i] = indexFirstElement;
+                }
+                else if (i % 2 == 1)
+                {
+                    firstArr[i] = indexFirstElement;
+                    secondArr[i] = indexZeroElement;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", firstArr));
+            Console.WriteLine(string.Join(" ", secondArr));
+        }
+    }
+}
