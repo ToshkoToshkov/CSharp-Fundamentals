8391d147cdbe29d925cb3b20c69b004a313598c5
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-13 13:48:49 +0200

    Add files via upload

---
+++ 05.Methods-Lab/07.GreaterOfTwoValues/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace GreaterOfTwoValues2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+
+            switch (type)
+            {
+                case "int":
+                    int first = int.Parse(Console.ReadLine());
+                    int second = int.Parse(Console.ReadLine());
+                    Console.WriteLine(Bigger(first, second));
+                    break;
+
+                case "char":
+                    char firstChar = char.Parse(Console.ReadLine());
+                    char secondChar = char.Parse(Console.ReadLine());
+                    Console.WriteLine(BiggerChar(firstChar, secondChar));
+                    break;
+
+                case "string":
+                    string firstString = Console.ReadLine();
+                    string secondString = Console.ReadLine();
+                    Console.WriteLine(BiggerString(firstString, secondString));
+                    break;
+            }
+
+        }
+
+        public static int Bigger(int first, int second)
+        {
+            if (first > second)
+            {
+                return first;
+            }
+            else
+            {
+                return second;
+            }
+        }
+
+        public static char BiggerChar(char firstChar, char secondChar)
+        {
+            if (firstChar > secondChar)
+            {
+                return firstChar;
+            }
+            else
+            {
+                return secondChar;
+            }
+        }
+
+        public static string BiggerString(string firstString, string secondString)
+        {
+            if (firstString.CompareTo(secondString) >= 0)
+            {
+                return firstString;
+            }
+            else
+            {
+                return secondString;
+            }
+        }
+    }
+}
