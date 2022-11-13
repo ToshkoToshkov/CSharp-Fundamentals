9469b7e229c60003d14946f5ce870e196aa17b52
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-13 14:02:07 +0200

    Add files via upload

---
+++ 06.Methods-Exercises/03.CharactersInRange/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CharactersInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char first = char.Parse(Console.ReadLine());
+            char second = char.Parse(Console.ReadLine());
+
+            PrintCharInRange(first, second);
+        }
+
+        private static void PrintCharInRange(char first, char second)
+        {
+            if (first > second)
+            {
+                char firstChar = first;
+                first = second;
+                second = firstChar;
+            }
+
+            for (int i = first + 1; i < second; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+        }
+    }
+}
