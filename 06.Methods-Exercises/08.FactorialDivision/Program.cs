c8d7c0e5050f70920f6cc62c55a940ffd07ce46c
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-13 14:04:49 +0200

    Add files via upload

---
+++ 06.Methods-Exercises/08.FactorialDivision/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FactorialDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+
+            double factorialFirstNum = GetFactoriel(first);
+            double factorialSecondNum = GetFactoriel(second);
+
+            double result = factorialFirstNum / factorialSecondNum;
+
+            Console.WriteLine($"{result:F2}");
+        }
+
+        public static double GetFactoriel(int number)
+        {
+            double result = 1;
+            while (number != 1)
+            {
+                result = result * number;
+                number = number - 1;
+            }
+            return result;
+        }
+    }
+}
