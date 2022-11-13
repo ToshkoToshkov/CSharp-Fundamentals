f3dfddfc2a7cbfca9fb898a1df9c20d5349b77c9
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-13 14:03:42 +0200

    Add files via upload

---
+++ 06.Methods-Exercises/06.MiddleCharacters/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace MiddleCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string output = string.Empty;
+
+            if (input.Length % 2 == 0)
+            {
+                output = GetMiddleCharFromEvenString(input);
+            }
+            else
+            {
+                output = GetMiddleCharFromOddString(input);
+            }
+
+            Console.WriteLine(output);
+        }
+
+        private static string GetMiddleCharFromOddString(string input)
+        {
+            int index = input.Length / 2;
+            string chars = input.Substring(index, 1);
+            return chars;
+        }
+
+        private static string GetMiddleCharFromEvenString(string input)
+        {
+            int index = input.Length / 2;
+            string chars = input.Substring(index - 1, 2);
+            return chars;
+        }
+    }
+}
