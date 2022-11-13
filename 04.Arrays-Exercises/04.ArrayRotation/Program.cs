d1f09af29c5b46b677e79169e4929bb8ad8b407a
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-13 12:56:48 +0200

    Add files via upload

---
+++ 04.Arrays-Exercises/04.ArrayRotation/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ArrayRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(" ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string elementsToRotate = arr[0];
+
+                for (int j = 1; j < arr.Length; j++)
+                {
+                    string currentElement = arr[j];
+                    arr[j - 1] = currentElement;
+                }
+
+                arr[arr.Length - 1] = elementsToRotate;
+            }
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
