5e43c337eddf980a1b5253e9b266901fbc219bd6
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-13 12:52:44 +0200

    Add files via upload

---
+++ 03.Arrays-Lab/07.EqualArrays/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace EqualArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] secondArr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = 0;
+
+            for (int i = 0; i < firstArr.Length; i++)
+            {
+
+                if (firstArr[i] != secondArr[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    break;
+                }
+                else
+                {
+                    for (int j = 0; j < firstArr.Length; j++)
+                    {
+                        sum += firstArr[j];   
+                    }
+                        Console.WriteLine($"Arrays are identical. Sum: {sum}");
+                    break;
+                }
+            }
+           
+        }
+    }
+}
