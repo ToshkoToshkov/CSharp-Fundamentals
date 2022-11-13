b78280cb68fdf1683d783d8dd76596bd95529aca
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-13 12:50:57 +0200

    Add files via upload

---
+++ 03.Arrays-Lab/02.PrintNumbersInReverseOrder/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PrintNumbersInReversOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] number = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                number[i] = currentNum;
+            }
+
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                Console.Write(number[i] + " ");
+            }
+        }
+    }
+}
