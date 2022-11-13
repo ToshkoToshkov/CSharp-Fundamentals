aaaf99543dceae46d54804b35e2b4d037e7de598
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-13 13:48:18 +0200

    Add files via upload

---
+++ 05.Methods-Lab/06.RepeatString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace RepeatString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+
+            RepeatString(text, count);
+        }
+
+        private static void RepeatString(string str, int num)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < num; i++)
+            {
+                result.Append(str);
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+
+    }
+}
