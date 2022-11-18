eb976429abf91f370be24688315ed3437c82819f
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-18 17:47:39 +0200

    Add files via upload

---
+++ 09.ObjectAndClasses-Lab/04.Students/Program.cs
@@ -0,0 +1,35 @@
+﻿
+List<Student> students = new List<Student>(); 
+
+string input = Console.ReadLine();
+
+while (input != "end")
+{
+    string[] currInput = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    string firstName = currInput[0];
+    string lastName = currInput[1];
+    int age = int.Parse(currInput[2]);
+    string hometown = currInput[3];
+
+    Student student = new Student()
+    {
+        FirstName = firstName,
+        LastName = lastName,
+        Age = age,
+        Hometown = hometown
+    };
+
+    students.Add(student);
+    input = Console.ReadLine();
+}
+
+string filteredCity = Console.ReadLine();
+
+foreach (var student in students)
+{
+    if (student.Hometown == filteredCity)
+    {
+        Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+    }
+}
---
+++ 09.ObjectAndClasses-Lab/04.Students/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace _04.Students
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public string Hometown { get; set; }
+    }
+}
