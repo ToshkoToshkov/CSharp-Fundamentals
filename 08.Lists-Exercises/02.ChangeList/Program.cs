a2666fe737a901908f84e69a87341bfc2884f7d3
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-18 16:26:50 +0200

    Add files via upload

---
+++ 08.Lists-Exercises/02.ChangeList/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ChangeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                string[] comArr = command.Split();
+
+                string firstCommand = comArr[0];
+                int element = int.Parse(comArr[1]);
+
+                if (firstCommand == "Delete")
+                {
+                    nums.RemoveAll(x => x == element);
+                }
+                else if (firstCommand == "Insert")
+                {
+                    int indexer = int.Parse(comArr[2]);
+                    nums.Insert(indexer, element);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
