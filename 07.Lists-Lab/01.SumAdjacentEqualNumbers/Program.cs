05ba0735ffb01350e802e0ff7143181b29d8afc8
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-18 16:18:26 +0200

    Add files via upload

---
+++ 07.Lists-Lab/01.SumAdjacentEqualNumbers/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Lists_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToList();
+
+            GetResult(numbers);
+        }
+
+        static void GetResult(List<double> numbers)
+        {
+
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int nextIndex = 0;
+
+                if (i + 1 > numbers.Count - 1)
+                {
+                    break;
+                }
+                else
+                {
+                    nextIndex = i + 1;
+                }
+
+                if (numbers[i] == numbers[nextIndex])
+                {
+                    numbers[i] += numbers[nextIndex];
+                    numbers.RemoveAt(nextIndex);
+                    i = -1;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+
+        }
+    }
+}
