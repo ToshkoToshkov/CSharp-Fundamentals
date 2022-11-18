4b249711fcaeaf83f81c80a079d82e42393762c7
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-18 16:21:58 +0200

    Add files via upload

---
+++ 07.Lists-Lab/04.RemoveNEgativesAndReverse/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace RemoveNegativesAndReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] < 0)
+                {
+                    nums.RemoveAt(i--);
+                }
+            }
+
+            nums.Reverse();
+
+            if (nums.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", nums));
+            }
+        }
+    }
+}
