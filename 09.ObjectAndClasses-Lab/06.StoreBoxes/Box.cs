ed7efb1b5421040318f65686f5cb45fabab4fa87
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-18 17:49:20 +0200

    Add files via upload

---
+++ 09.ObjectAndClasses-Lab/06.StoreBoxes/Box.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.StoreBoxes
+{
+    internal class Box
+    {
+        public Box()
+        {
+            Item = new Item();
+        }
+
+        public string SerialNumber { get; set; }
+
+        public Item Item { get; set; }
+
+        public int ItemQuantity { get; set; }
+
+        public decimal PriceForBox { get; set; }
+
+    }
+}
---
+++ 09.ObjectAndClasses-Lab/06.StoreBoxes/Program.cs
@@ -0,0 +1,55 @@
+﻿using _06.StoreBoxes;
+using System.Text;
+
+string input = Console.ReadLine();
+
+List<Box> boxes = new List<Box>();
+
+while (input != "end")
+{
+    string[] currCommand = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+    string serialNumber = currCommand[0];
+    string itemName = currCommand[1];
+    int itemQuantity = int.Parse(currCommand[2]);
+    decimal itemPrice = decimal.Parse(currCommand[3]);
+    
+    //decimal priceOfOneBox = itemQuantity * itemPrice;
+
+    Item item = new Item()
+    {
+        Name = itemName,
+        Price = itemPrice
+    };
+
+    Box box = new Box()
+    {
+        SerialNumber = serialNumber,
+        Item = item,
+        ItemQuantity = itemQuantity,
+        PriceForBox = itemQuantity * itemPrice
+    };
+
+    boxes.Add(box);
+
+    input = Console.ReadLine();
+}
+
+List<Box> orderedBoxes = boxes.OrderByDescending(p => p.PriceForBox).ToList();
+ 
+StringBuilder sb = new StringBuilder();
+
+string result = "";
+
+foreach (var box in orderedBoxes)
+{
+    sb.AppendLine(box.SerialNumber);
+    sb.AppendLine($@"-- {box.Item.Name} - ${box.Item.Price:f2}: {box.ItemQuantity}");
+    sb.AppendLine($@"--${box.PriceForBox:f2}");
+
+    result = sb.ToString();
+}
+
+Console.WriteLine(result);
+
+
