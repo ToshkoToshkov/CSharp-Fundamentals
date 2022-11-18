8aa52144ca4eff91863d0c6ad31a3c073faf3d6d
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-18 16:19:22 +0200

    Add files via upload

---
+++ 07.Lists-Lab/02.MergingLists/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace MergingLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> first = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> second = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < Math.Min(first.Count, second.Count); i++)
+            {
+                result.Add(first[i]);
+                result.Add(second[i]);
+            }
+
+            for (int i = Math.Min(first.Count, second.Count);
+                i < Math.Max(first.Count, second.Count); i++)
+            {
+                if (i >= first.Count)
+                {
+                    result.Add(second[i]);
+                }
+                else
+                {
+                    result.Add(first[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
