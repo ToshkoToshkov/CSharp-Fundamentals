872e5074a5ed49c5d9e1038ce0a41b165d483764
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-18 16:22:24 +0200

    Add files via upload

---
+++ 07.Lists-Lab/05.ListManipulationBasic/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ListManipulationBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "end")
+                {
+                    break;
+                }
+
+                string[] tokens = line.Split();
+
+                switch (tokens[0])
+                {
+                    case "Add":
+                        int numbersToAdd = int.Parse(tokens[1]);
+                        nums.Add(numbersToAdd);
+                        break;
+
+                    case "Remove":
+                        int numbersToRemove = int.Parse(tokens[1]);
+                        nums.Remove(numbersToRemove);
+                        break;
+
+                    case "RemoveAt":
+                        int indexToRemove = int.Parse(tokens[1]);
+                        nums.RemoveAt(indexToRemove);
+                        break;
+
+                    case "Insert":
+                        int numberToInsert = int.Parse(tokens[1]);
+                        int indexToInsert = int.Parse(tokens[2]);
+                        nums.Insert(indexToInsert, numberToInsert);
+                        break;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
