6b99aa2a2b753beb7a2b192bf486365d35f05beb
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-21 11:30:32 +0200

    Add files via upload

---
+++ 16.TextProcessing/08.LetterChangeNumbers/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace LetterChangeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            double result = 0;
+
+            string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string curr = input[i];
+                char first = curr[0];
+                char last = curr[curr.Length - 1];
+
+                double number = double.Parse(curr.Substring(1, curr.Length - 2));
+
+                int firstElementIndex = alpha.IndexOf(char.ToUpper(first));
+                int secondElementIndex = alpha.IndexOf(char.ToUpper(last));
+
+                if ((int)first >= 65 && (int)first <= 90)
+                {
+                    number = number / (firstElementIndex + 1);
+                }
+                else
+                {
+                    number = number * (firstElementIndex + 1);
+                }
+
+                if ((int)last >= 65 && (int)last <= 90)
+                {
+                    number = number - (secondElementIndex + 1);
+                }
+                else
+                {
+                    number = number + (secondElementIndex + 1);
+                }
+
+                result += number;
+            }
+
+            Console.WriteLine($"{result:F2}");
+        }
+    }
+}
