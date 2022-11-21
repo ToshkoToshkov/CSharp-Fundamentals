8c4ffdefa88e72ad80a0899775bc771ca58adec2
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-21 11:12:06 +0200

    Add files via upload

---
+++ 13.AssociativeArrays-Exercises/01.CountCharsInAStrings/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CountCharsInAStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+
+            Dictionary<char, int> occurrences = new Dictionary<char, int>();
+
+            foreach (var item in input)
+            {
+                if (item != ' ')
+                {
+                    if (!occurrences.ContainsKey(item))
+                    {
+                        occurrences.Add(item, 0);
+                    }
+                    occurrences[item]++;
+                }
+            }
+
+            foreach (var item in occurrences)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
