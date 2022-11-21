c2e406364d3fe69e9c313a0944e0a3797d489d8f
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-21 11:16:43 +0200

    Add files via upload

---
+++ 14.RegularExpresions-Lab/01.MatchFullName/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]+ \b[A-Z][a-z]+";
+            string text = Console.ReadLine();
+
+            MatchCollection matchedNames = Regex.Matches(text, pattern);
+
+            foreach (Match name in matchedNames)
+            {
+                Console.Write(name.Value + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
