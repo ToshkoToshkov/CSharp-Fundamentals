60fe6f17b0998552add79f802aeb68f491049f9c
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-21 11:21:44 +0200

    Add files via upload

---
+++ 15.ExamPreparations/04.PasswordReset/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace PasswordReset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            StringBuilder newRawPAssword = new StringBuilder();
+
+            string command = Console.ReadLine();
+
+
+            while (command != "Done")
+            {
+                string[] tokens = command
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string cmd = tokens[0];
+
+                switch (cmd)
+                {
+                    case "TakeOdd":
+                        char[] chars = input.ToCharArray();
+
+                        for (int i = 1; i < input.Length; i += 2)
+                        {
+                            newRawPAssword.Append(input[i]);
+                        }
+                        input = newRawPAssword.ToString();
+                        Console.WriteLine(input);
+
+                        break;
+
+                    case "Cut":
+
+                        int index = int.Parse(tokens[1]);
+                        int lenght = int.Parse(tokens[2]);
+
+                        input = input.Remove(index, lenght);
+                        Console.WriteLine(input);
+
+
+                        break;
+
+                    case "Substitute":
+                        string substring = tokens[1];
+                        string substitute = tokens[2];
+
+                        if (input.Contains(substring))
+                        {
+                            input = input.Replace(substring, substitute);
+                            Console.WriteLine(input);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nothing to replace!");
+                        }
+
+                        break;
+                }
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Your password is: {input}");
+        }
+    }
+}
