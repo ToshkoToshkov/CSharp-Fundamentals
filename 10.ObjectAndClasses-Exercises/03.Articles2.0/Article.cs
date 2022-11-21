5a79e6d3b8daedec9e4a44bb11187d52fefb6a0c
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-21 09:06:42 +0200

    Add files via upload

---
+++ 10.ObjectAndClasses-Exercises/03.Articles2.0/Article.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Articles
+{
+    public class Article
+    {
+    
+        public Article(string title, string content, string author)
+        {
+            this.Title = title;
+            this.Content = content;
+            this.Author = author;
+        }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+        public override string ToString()
+        {
+            string result = $"{this.Title} - {this.Content}: {this.Author}";
+
+            return result.ToString();
+        }
+
+    }
+}
---
+++ 10.ObjectAndClasses-Exercises/03.Articles2.0/Program.cs
@@ -0,0 +1,41 @@
+﻿using _02.Articles;
+
+int numOfArticles = int.Parse(Console.ReadLine());
+
+List<Article> articles = new List<Article>();
+
+for (int i = 0; i < numOfArticles; i++)
+{
+    string[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+    string title = input[0];
+    string content = input[1];
+    string author = input[2];
+
+    Article article = new Article(title, content, author);
+    articles.Add(article);
+
+    
+}
+
+string wordToOrder = Console.ReadLine();
+
+List<Article> orderedArticles = new List<Article>();
+
+if (wordToOrder == "title")
+{
+    orderedArticles = articles.OrderBy(x => x.Title).ToList();
+}
+else if (wordToOrder == "content")
+{
+    orderedArticles = articles.OrderBy(x => x.Content).ToList();
+}
+else if (wordToOrder == "author")
+{
+    orderedArticles = articles.OrderBy(x => x.Author).ToList();
+}
+
+foreach (var item in orderedArticles)
+{
+    Console.WriteLine(item.ToString()); 
+}
