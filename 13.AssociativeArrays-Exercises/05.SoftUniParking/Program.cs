4b088fea041904d179d4d408590a69b6bc6201d0
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-21 11:14:30 +0200

    Add files via upload

---
+++ 13.AssociativeArrays-Exercises/05.SoftUniParking/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace SoftUniParking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, string> outut = new Dictionary<string, string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] cmdArg = Console.ReadLine().Split();
+                string task = cmdArg[0];
+                string username = cmdArg[1];
+
+                if (task == "register")
+                {
+                    string licensePlateNumber = cmdArg[2];
+
+                    if (!outut.ContainsKey(username))
+                    {
+                        outut.Add(username, licensePlateNumber);
+                        Console.WriteLine($"{username} registered {licensePlateNumber} successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {licensePlateNumber}");
+                    }
+                }
+                else if (task == "unregister")
+                {
+                    if (!outut.ContainsKey(username))
+                    {
+                        Console.WriteLine($"ERROR: user {username} not found");
+                    }
+                    else
+                    {
+                        outut.Remove(username);
+                        Console.WriteLine($"{username} unregistered successfully");
+                    }
+                }
+            }
+
+            foreach (var item in outut)
+            {
+                Console.WriteLine($"{item.Key} => {item.Value}");
+            }
+        }
+
+    }
+}
