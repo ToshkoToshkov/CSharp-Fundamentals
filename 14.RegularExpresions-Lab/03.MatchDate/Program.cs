17cd45af25a32e085b538b3186f84dce51c517e7
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-21 11:17:39 +0200

    Add files via upload

---
+++ 14.RegularExpresions-Lab/03.MatchDate/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+var regex = @"\b(?<day>\d{2})([-.\/])(?<month>[A-Z][a-z]{2})\2(?<year>\d{4})\b";
+
+var datesStrings = Console.ReadLine();
+
+var dates = Regex.Matches(datesStrings, regex);
+
+foreach (Match date in dates)
+{
+    var day = date.Groups["day"].Value;
+    var month = date.Groups["month"].Value;
+    var year = date.Groups["year"].Value;
+
+    Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+}
