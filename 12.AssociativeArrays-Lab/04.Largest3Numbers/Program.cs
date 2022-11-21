d1fe9c4b024545d19f79cc2248f29df6482fc029
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-21 10:51:35 +0200

    Add files via upload

---
+++ 12.AssociativeArrays-Lab/04.Largest3Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] input = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int[] sortedNumbers = input.OrderByDescending(n => n).ToArray();
+
+for (int i = 0; i < 3; i++)
+{
+    Console.Write(sortedNumbers[i] + " ");
+}
+
