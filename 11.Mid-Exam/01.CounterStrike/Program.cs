1ce024aef2b1adfa5c7a450f8986d2a94f6a56b4
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-21 10:44:01 +0200

    Add files via upload

---
+++ 11.Mid-Exam/01.CounterStrike/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CounterStrike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+
+            string command = Console.ReadLine();
+
+            int countWins = 0;
+
+            while (command != "End of battle")
+            {
+                int intCommand = int.Parse(command);
+
+                if (energy < intCommand)
+                {
+                    Console.WriteLine($"Not enough energy!" +
+                        $" Game ends with {countWins}" +
+                        $" won battles and {energy} energy");
+                    break;
+                }
+
+                countWins++;
+
+                if (countWins % 3 == 0)
+                {
+                    energy += countWins;
+                }
+
+                energy -= intCommand;
+
+                command = Console.ReadLine();
+
+            }
+
+            if (command == "End of battle")
+            {
+                Console.WriteLine($"Won battles: {countWins}. Energy left: {energy}");
+            }
+        }
+    }
+}
