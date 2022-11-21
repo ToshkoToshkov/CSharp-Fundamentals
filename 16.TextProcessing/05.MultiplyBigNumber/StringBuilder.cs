1bf07f92ebc9814809c503b7e6b090302a9e7aaa
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-21 11:28:47 +0200

    Add files via upload

---
+++ 16.TextProcessing/05.MultiplyBigNumber/StringBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MultiplyBigNumber
+{
+    public class StringBuilder
+    {
+        public StringBuilder()
+        {
+        }
+
+        public void insert(int v, int result)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
