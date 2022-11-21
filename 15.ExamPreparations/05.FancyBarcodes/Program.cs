cab477a5f7e07d0c74d22948d94c877b5fb767e1
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-21 11:22:22 +0200

    Add files via upload

---
+++ 15.ExamPreparations/05.FancyBarcodes/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace FancyBarcodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var regexValidBarcode = new Regex(@"^@#+(?<barcode>[A-Z][A-Za-z0-9]{4,}[A-Z])@#+$");
+            var numRegex = new Regex(@"\d");
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string str = Console.ReadLine();
+                var match = regexValidBarcode.Match(str);
+
+                if (match.Success)
+                {
+                    string name = match.Groups["barcode"].Value;
+                    var digitMatche = numRegex.Matches(name);
+                    string productGroup = string.Empty;
+
+                    foreach (Match digit in digitMatche)
+                    {
+                        if (digit.Success)
+                        {
+                            productGroup += digit.Value; 
+                        }
+                    }
+
+                    if (productGroup.Length == 0)
+                    {
+                        productGroup = "00";
+                    }
+
+                    Console.WriteLine($"Product group: {productGroup}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+
+            }
+
+        }
+    }
+}
