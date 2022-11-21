f1408e453d2ce4dee11d9d0b6728a489b79bfaf0
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-21 11:20:56 +0200

    Add files via upload

---
+++ 15.ExamPreparations/02.EmojiDetector/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.RegularExpressions;
+
+namespace EmojiDetector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberPattern = @"\d";
+            string emojiPattern = @"(:{2}|\*{2})[A-Z][a-z]{2,}\1";
+            Regex numreg = new Regex(numberPattern);
+            Regex emojiReg = new Regex(emojiPattern);
+
+            string text = Console.ReadLine();
+
+            long coolTreshold = 1;
+            numreg.Matches(text)
+                .Select(m => m.Value)
+                .Select(int.Parse)
+                .ToList()
+                .ForEach(x => coolTreshold *= x);
+
+            var matches = emojiReg.Matches(text);
+            int totalEmojis = matches.Count;
+            List<string> coolEmojis = new List<string>();
+
+            foreach (Match item in matches)
+            {
+                long coolIndex = item.Value
+                    .Substring(2, item.Value.Length - 4)
+                    .ToCharArray()
+                    .Sum(x => (int)x);
+
+                if (coolIndex > coolTreshold)
+                {
+                    coolEmojis.Add(item.Value);
+                }
+            }
+
+            Console.WriteLine($"Cool threshold: {coolTreshold}");
+            Console.WriteLine($"{totalEmojis} emojis found in the text. The cool ones are:");
+            foreach (var item in coolEmojis)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
