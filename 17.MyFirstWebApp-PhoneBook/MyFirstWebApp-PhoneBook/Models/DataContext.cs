887f39712a035f618919705dd9617562201b7947
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-21 11:42:05 +0200

    Add files via upload

---
+++ 17.MyFirstWebApp-PhoneBook/MyFirstWebApp-PhoneBook/Models/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace MyFirstWebApp_PhoneBook.Models
+{
+    public class DataContext
+    {
+        public static List<Contact> Contacts { get; set; }
+    }
+}
