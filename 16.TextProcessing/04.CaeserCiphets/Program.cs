460927f7aea9f29ceee38955b3f987815ed30b5a
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-21 11:28:19 +0200

    Add files via upload

---
+++ 16.TextProcessing/04.CaeserCiphets/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CaeserCiphets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            foreach (char ch in input)
+            {
+                var currentChar = (Char)(ch + 3);
+                Console.Write(currentChar);
+            }
+        }
+    }
+}
