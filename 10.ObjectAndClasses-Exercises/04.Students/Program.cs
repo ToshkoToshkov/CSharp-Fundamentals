e61be097b9b2bade256b45dddb032ef247399706
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-21 09:07:54 +0200

    Add files via upload

---
+++ 10.ObjectAndClasses-Exercises/04.Students/Program.cs
@@ -0,0 +1,34 @@
+﻿using _04.Students;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+int countOfStudents = int.Parse(Console.ReadLine());
+
+List<Student> students = new List<Student>();
+
+for (int i = 0; i < countOfStudents; i++)
+{
+    string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    string firstName = input[0];
+    string secondName = input[1];
+    double grade = double.Parse(input[2]);
+
+    Student student = new Student()
+    {
+        FirstName = firstName,
+        SecondName = secondName,
+        Grade = grade
+    };
+
+    students.Add(student);
+}
+
+List<Student> orderedStudents = new List<Student>();
+
+orderedStudents = students.OrderByDescending(x => x.Grade).ToList();
+
+foreach (Student student in orderedStudents)
+{
+    Console.WriteLine($"{student.FirstName} {student.SecondName}: {student.Grade}");
+}
---
+++ 10.ObjectAndClasses-Exercises/04.Students/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Students
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+
+        public string SecondName { get; set; }
+
+        public double Grade { get; set; }
+
+    }
+}
