468005a0eb06b1d7645ff37574fe2a12a3174e79
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-21 10:50:48 +0200

    Add files via upload

---
+++ 12.AssociativeArrays-Lab/03.WordSynonyms/Program.cs
@@ -0,0 +1,27 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+var counts = new Dictionary<string, List<string>>();
+
+string key = string.Empty;
+
+for (int i = 1; i <= 2 * n; i++)
+{
+    string input = Console.ReadLine();
+    if (i % 2 != 0)
+    {
+        key = input;
+
+        if (!(counts.ContainsKey(input)))
+        {
+            counts.Add(input, new List<string>());
+        }
+    }
+    else
+    {
+        counts[key].Add(input);
+    }
+}
+foreach (var word in counts)
+{
+    Console.WriteLine($"{word.Key} - {string.Join(", ", word.Value)}");
+}
