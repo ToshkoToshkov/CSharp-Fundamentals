50dea07fab956432c4388f61815181360c6d112f
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-21 11:17:04 +0200

    Add files via upload

---
+++ 14.RegularExpresions-Lab/02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+string pattern = @"(\+359([ -])2(\2)(\d{3})(\2)(\d{4}))\b";
+string phones = Console.ReadLine();
+
+MatchCollection phoneMatches = Regex.Matches(phones, pattern);
+
+string[] matchPhones = phoneMatches
+    .Cast<Match>()
+    .Select(x => x.Value.Trim())
+    .ToArray();
+
+Console.WriteLine(string.Join(", ", matchPhones));
