37dc747f43fd7f7c17d490cb1127791246998bdb
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-21 09:06:07 +0200

    Add files via upload

---
+++ 10.ObjectAndClasses-Exercises/02.Articles/Article.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Articles
+{
+    public class Article
+    {
+    
+        public Article(string title, string content, string author)
+        {
+            this.Title = title;
+            this.Content = content;
+            this.Author = author;
+        }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+        public string Edit(string newContent)
+        {
+            return this.Content = newContent; 
+        }
+
+        public string ChangeAuthor(string newAuthor)
+        {
+            return this.Author = newAuthor;
+        }
+
+        public string Rename(string newTitle)
+        {
+            return this.Title = newTitle;
+        }
+
+        public override string ToString()
+        {
+            string result = $"{this.Title} - {this.Content}: {this.Author}";
+
+            return result.ToString();
+        }
+
+    }
+}
---
+++ 10.ObjectAndClasses-Exercises/02.Articles/Program.cs
@@ -0,0 +1,33 @@
+﻿using _02.Articles;
+
+string[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+int numOfCommands = int.Parse(Console.ReadLine());
+
+string title = input[0];
+string content = input[1];
+string author = input[2];
+
+Article article = new Article(title, content, author);
+
+for (int i = 0; i < numOfCommands; i++)
+{
+    string[] command = Console.ReadLine().Split(": ", StringSplitOptions.RemoveEmptyEntries);
+
+    string cmd = command[0];
+    string newContent = command[1];
+
+    if (cmd == "Edit")
+    {
+        article.Edit(newContent);
+    }
+    else if (cmd == "ChangeAuthor")
+    {
+        article.ChangeAuthor(newContent);
+    }
+    else if (cmd == "Rename")
+    {
+        article.Rename(newContent);
+    }
+}
+
+Console.WriteLine(article.ToString().Trim());
