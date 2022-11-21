46a5bb443719affcedb05cbae1b289437e487202
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-21 11:25:28 +0200

    Add files via upload

---
+++ 16.TextProcessing/01.ValidUsernames/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace ValidUSernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var curr = input[i];
+                if (IsValid(curr))
+                {
+                    Console.WriteLine(curr);
+                }
+            }
+        }
+
+        public static bool IsValid(string current)
+        {
+            return current.Length > 3 &&
+                current.Length < 16 &&
+                current.All(c => char.IsLetterOrDigit(c)) ||
+                current.Contains("-") ||
+                current.Contains("_");
+        }
+    }
+}
