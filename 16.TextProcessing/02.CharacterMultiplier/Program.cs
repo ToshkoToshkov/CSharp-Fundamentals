d4ab9c3863cc94765226a811542803aa2c980c8f
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-21 11:26:06 +0200

    Add files via upload

---
+++ 16.TextProcessing/02.CharacterMultiplier/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace CharacterMultiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tokens = Console.ReadLine().Split();
+
+            var firstWord = tokens[0];
+            var secondWord = tokens[1];
+
+            var longestWord = firstWord;
+            var shortestWord = secondWord;
+
+            if (firstWord.Length < secondWord.Length)
+            {
+                longestWord = secondWord;
+                shortestWord = firstWord;
+            }
+
+            var total = CharManipulator(longestWord, shortestWord);
+            Console.WriteLine(total);
+        }
+
+        public static int CharManipulator(string longestWord, string shortesWord)
+        {
+            var sum = 0;
+
+            for (int i = 0; i < shortesWord.Length; i++)
+            {
+                var multiply = longestWord[i] * shortesWord[i];
+                sum += multiply;
+            }
+
+            for (int i = shortesWord.Length; i < longestWord.Length; i++)
+            {
+                sum += longestWord[i];
+            }
+            return sum;
+        }
+    }
+}
