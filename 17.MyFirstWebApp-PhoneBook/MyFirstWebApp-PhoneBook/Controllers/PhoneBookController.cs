280f68b0a2f5b0fefeee5aafef88ba78d94ef286
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-21 11:40:59 +0200

    Add files via upload

---
+++ 17.MyFirstWebApp-PhoneBook/MyFirstWebApp-PhoneBook/Controllers/PhoneBookController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstWebApp_PhoneBook.Models;
+
+namespace MyFirstWebApp_PhoneBook.Controllers
+{
+    public class PhoneBookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(DataContext.Contacts);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                DataContext.Contacts.Add(model);
+            }
+            else
+            {
+                TempData["Error"] = "Incorrect data!";
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
